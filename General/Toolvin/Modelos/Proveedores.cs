3e142828ff49db145fa013ab3ac58058461c8521
Juan José Arreola Gonzalez <81401889+ElHuachi@users.noreply.github.com>
2023-04-18 00:50:53 -0600

    Add files via upload

---
+++ General/Toolvin/Modelos/Proveedores.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    public class Proveedores
+    {
+        //Generar los atributos
+        public int id { get; set; }
+        public string proveedor { get; set; }
+        public string correo { get; set; }
+        public string telefono { get; set; }
+        public string direccion { get; set; }
+        public string descripcion { get; set; }
+        public int activo { get; set; }
+
+        //Crear un constructor vacio
+        public Proveedores()
+        {
+
+        }
+    }
+}
---
+++ General/Toolvin/Modelos/Ventas.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    public class Ventas
+    {
+        public int IdCliente { get; set; }
+        public int IdUsuario { get; set; }
+        public DateTime Fecha { get; set; }
+        public double Subtotal { get; set; }
+        public double Iva { get; set; }
+        public double Total { get; set; }
+        public bool Activo { get; set; }
+        public string Cliente { get; set; }
+        public string Usuario { get; set; }
+
+        public Ventas() { }
+    }
+}
